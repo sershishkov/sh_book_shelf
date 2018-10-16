df37f12a712165118681e19e2ab800d12c20895a
sershishkov <ser.shishkov@gmail.com>
2018-10-16 13:07:02 +0300

    16-146

--- server/server.js
+++ server/server.js
@@ -20,13 +20,49 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 ////////////////////////////////////////////////////////////
 
- 
+//GET//
+app.get('/api/getBook',(req,res)=>{
+  let id = req.query.id;
+  Book.findById(id,(err, doc) =>{
+    if(err) res.status(400).send(err);
+    res.send(doc);
+  });
+});
+app.get('/api/books',(req,res)=>{
+  //localhost:3001/api/books?skip=3&limit=2&order=asc
+  let skip = parseInt(req.query.skip); 
+  let limit = parseInt(req.query.limit); 
+  let order = req.query.order; 
+//Oreder = asc || desc
+  Book.find()
+    .skip(skip)
+    .sort({_id:order})
+    .limit(limit)
+    .exec((err, doc) =>{
+      if(err) res.status(400).send(err);
+      res.send(doc);
+    });
+  
+});
+//POST//
+app.post('/api/book',(req,res)=>{
+  const book = new Book(req.body);
+  book.save((err,doc)=>{
+    if(err) res.status(400).send(err);
+    res.status(200).json({
+      post:true,
+      bookId:doc._id
+    });
+  });
+});
+//UPDATE//
+app.post('/api/book',(req,res)=>{
 
+});
+//DELETE//
+app.delete('/api/book',(req,res)=>{
 
-
-
-
-
+});
 
 
 ///////////////////////////////////////////////////////////
