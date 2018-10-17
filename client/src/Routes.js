9224e6742a0481eb1615136e11becbcee4aedcd1
sershishkov <ser.shishkov@gmail.com>
2018-10-17 12:36:52 +0300

    16-152

---
+++ client/src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route }from 'react-router-dom';
+
+const Routes = () => {
+  return (
+    <div>
+      Hello from routes
+    </div>
+  );
+};
+
+export default Routes;
