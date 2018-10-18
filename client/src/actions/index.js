233c459a2e4183f991bd46bc2bdf8317b21a19a3
sershishkov <ser.shishkov@gmail.com>
2018-10-18 11:14:05 +0300

    16-156

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,18 +1,26 @@
 import axios from 'axios';
 
 export function getBooks (
-  limit =10,
+  limit = 10,
   start = 0,
-  order = 'asc'
+  order = 'asc',
+  list = ''
 ){
   const request = 
        axios.get(`/api/books?limit=${limit}&skip=${start}&order=${order}`)
-            .then(response => response.data)
-            .catch(err => console.log(err));      
+            .then(response => {
+              if(list){
+                return [...list, ...response.data];
+              }else{
+                return response.data;
+              }
+            })
+            .catch(err => console.log(err));  
 
       return{
         type:'GET_BOOKS',
         payload:request
+
       }
 
 };
--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -2,24 +2,37 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getBooks } from '../actions';
 
+import BookItem from '../widgetsUI/BookItem';
+
  class HomeContainer extends Component {
 
   componentWillMount(){
-    this.props.dispatch(getBooks(6,0,'desc'));
+    this.props.dispatch(getBooks(1,0,'desc'));
   }
 
   renderItems = (books)=>(
     books.list?
       books.list.map((item, i)=>(
-        'item '
+        <BookItem {...item} key={i}/>
       ))
     :null
   )
+  loadmore = ()=>{
+    let count = this.props.books.list.length;
+    console.log(count);
+    this.props.dispatch(getBooks(1,count,'desc', this.props.books.list));
+
+  }
 
   render() {
+    // console.log(this.props);
     return (
       <div>
         {this.renderItems(this.props.books)}
+        <div 
+          className="loadmore"
+          onClick={this.loadmore}
+        >Load more</div>
       </div>
     )
   }
