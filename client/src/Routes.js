a7073a02fee646df96f41b26056b0dcd103b9ea6
sershishkov <ser.shishkov@gmail.com>
2018-10-19 11:41:17 +0300

    16-161

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import BookView from './components/books';
 import Layout from './hoc/Layout';
 import Login from './containers/admin/Login';
+import User from './components/admin';
 
 import Auth from './hoc/Auth';
 
@@ -13,9 +14,10 @@ const Routes = () => {
   return (
     <Layout>
       <Switch>
-        <Route path="/" exact component={Auth(Home)} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/books/:id" exact component={BookView} />
+        <Route path="/" exact component={Auth(Home,null)} />
+        <Route path="/login" exact component={Auth(Login,false)} />
+        <Route path="/user" exact component={Auth(User,true)} />
+        <Route path="/books/:id" exact component={Auth(BookView)} />
       </Switch>
     </Layout>
   );
--- client/src/hoc/Auth.js
+++ client/src/hoc/Auth.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { auth } from '../actions'
 import { connect } from 'react-redux';
 
-export default function(ComposedClass){
+export default function(ComposedClass, reload ){
   class AuthenticationCheck extends Component {
     state ={
       loading:true
@@ -15,18 +15,24 @@ export default function(ComposedClass){
     componentWillReceiveProps(nextProps){
       this.setState({loading:false});
       if(!nextProps.user.login.isAuth){
-
-      }else{
+        if(reload){
+          this.props.history.push('/login');
+        }
         
+      }else{
+        if(reload === false){
+          this.props.history.push('/user');
+        }
       }
     }
 
     render(){
+      // console.log(this.props);
       if(this.state.loading){
         return <div className="loader">Loading...</div>
       }
       return(
-       <ComposedClass {...this.props} user=""/>
+       <ComposedClass {...this.props} user={this.props.user}/>
       )
     }
   }
