3cb07d8f2edb029b91042b5d0ec9ec218596bec0
sershishkov <ser.shishkov@gmail.com>
2018-10-22 13:00:43 +0300

    16-171

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -10,6 +10,7 @@ import AddReview from './containers/admin/AddRewiew';
 import UserPosts from './components/admin/UserPosts';
 import EditReview from './containers/admin/EditBook';
 import Register from './containers/admin/Register';
+import Logout from './components/admin/Logout';
 
 import Auth from './hoc/Auth';
 
@@ -20,7 +21,9 @@ const Routes = () => {
       <Switch>
         <Route path="/" exact component={Auth(Home,null)} />
         <Route path="/login" exact component={Auth(Login,false)} />
+        <Route path="/user/logout" exact component={Auth(Logout,true)} />
         <Route path="/user" exact component={Auth(User,true)} />
+
         <Route path="/user/register" exact component={Auth(Register,true)} />
         <Route path="/user/add" exact component={Auth(AddReview,true)} />
         <Route path="/user/edit-post/:id" exact component={Auth(EditReview,true)} />
